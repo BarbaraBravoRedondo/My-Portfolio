edc9b6c876b463920ae027a8389159a335fa68cd
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2024-10-10 15:41:08 +0200

    new project added to data projects

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url'; 
 import { dirname } from 'path';      
-import fs from 'fs';
-import path from 'path';
+
 
 
 const __filename = fileURLToPath(import.meta.url);
